aa7f1c5c83682efe306bec82d73fa17254b74556
Edson Tofolo <edsontofolo@gmail.com>
2023-02-02 09:24:51 -0300

    removido apiworker e ajustado retrofit instance

--- app/src/main/java/com/example/moblpr/clients/Retrofit.kt
+++ app/src/main/java/com/example/moblpr/clients/Retrofit.kt
@@ -1,14 +1,30 @@
 package com.example.moblpr.clients
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 class Retrofit {
 
     companion object {
-
         fun getInstance(path: String): retrofit2.Retrofit {
+
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .build()
+
             return retrofit2.Retrofit.Builder()
                 .baseUrl(path)
-                .addConverterFactory(ApiWorker.gsonConverter)
-                .client(ApiWorker.client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build()
         }
     }
